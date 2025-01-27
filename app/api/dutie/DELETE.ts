6001ff02e7bc5a91dbd3845ce4bb09f3260e9ad7
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2025-01-28 00:42:24 +0100

    dont care +1 having fun + playing

--- app/api/dutie/DELETE.ts
+++ app/api/dutie/DELETE.ts
@@ -1,62 +1,68 @@
-import { prisma } from '@/lib/prisma'
-import { getServerSession } from 'next-auth/next'
-import { authOptions } from '../auth/[...nextauth]/route'
-import { NextResponse } from 'next/server'
-import type { NextApiResponse } from 'next'
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { NextResponse } from "next/server";
+import type { NextApiResponse } from "next";
 
 interface req_body {
-    dutie_id: number
+  dutie_id: number;
 }
 
 export async function mDELETE(req: Request, res: NextApiResponse) {
-    const session = await getServerSession(authOptions)
-    if (!session || !session.user) {
-        return new NextResponse(JSON.stringify({ error: 'nie zautoryzowano' }), {
-            status: 401
-        })
-    }
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return new NextResponse(JSON.stringify({ error: "nie zautoryzowano" }), {
+      status: 401,
+    });
+  }
 
-    const body: req_body = await req.json()
-    if (!body || !body.dutie_id) {
-        return new NextResponse(JSON.stringify({ error: 'nie podano id obowiazku' }), {
-            status: 400
-        })
-    }
+  const body: req_body = await req.json();
+  if (!body || !body.dutie_id) {
+    return new NextResponse(
+      JSON.stringify({ error: "nie podano id obowiazku" }),
+      {
+        status: 400,
+      },
+    );
+  }
 
-    const dutie = await prisma.dutie.findFirst({
-        where: {
-            id: body.dutie_id,
-        }
-    })
+  const dutie = await prisma.dutie.findFirst({
+    where: {
+      id: body.dutie_id,
+    },
+  });
 
-    if (!dutie) {
-        return new NextResponse(JSON.stringify({ error: 'nie znaleziono obowiazku' }), {
-            status: 400
-        })
-    }
+  if (!dutie) {
+    return new NextResponse(
+      JSON.stringify({ error: "nie znaleziono obowiazku" }),
+      {
+        status: 400,
+      },
+    );
+  }
 
-    const house = await prisma.user_house.findFirst({
-        where: {
-            house_id: dutie.house_id,
-            profile: {
-                user_id: session.user.id
-            }
-        }
-    })
+  const house = await prisma.user_house.findFirst({
+    where: {
+      house_id: dutie.house_id,
+      profile: {
+        user_id: session.user.id,
+      },
+    },
+  });
 
-    if (!house) {
-        return new NextResponse(JSON.stringify({ error: 'nie należysz do domu' }), {
-            status: 400
-        })
-    }
+  if (!house) {
+    return new NextResponse(JSON.stringify({ error: "nie należysz do domu" }), {
+      status: 400,
+    });
+  }
 
-    await prisma.dutie.delete({
-        where: {
-            id: dutie.id
-        }
-    })
+  await prisma.dutie.delete({
+    where: {
+      id: dutie.id,
+    },
+  });
 
-    return new NextResponse(JSON.stringify(dutie), {
-        status: 200
-    })
+  return new NextResponse(JSON.stringify(dutie), {
+    status: 200,
+  });
 }
--- app/api/refresh/GET.ts
+++ app/api/refresh/GET.ts
@@ -1,32 +1,18 @@
-import { prisma } from '@/lib/prisma'
-import { getServerSession } from 'next-auth/next'
-import { authOptions } from '../auth/[...nextauth]/route'
-import { NextResponse } from 'next/server'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { getQuerryParameters } from '@/lib/getQuerryParameters'
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import type { NextApiResponse } from "next";
 
-
-export async function mGET(req: Request, res: NextApiResponse) {
-    console.info("REFRESH START")
-    const authHeader = req.headers.get('authorization');
-    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-        console.error("REFRESH ERROR UNAUTHORIZED")
-        return new Response('Unauthorized', {
-        status: 401,
-        });
-    }
-    try{
-        await prisma.user.findFirstOrThrow({
-            select:{
-                id: true
-            }
-        })
-    }
-    catch{
-        console.error("REFRESH QUERY ERROR")
-    }
-    console.info("REFRESH DONE")
-    return new NextResponse(JSON.stringify(true), {
-        status: 200
-    })
+export async function mGET(_req: Request, _res: NextApiResponse) {
+  console.info("REFRESH START");
+  let didRefresh;
+  try {
+    didRefresh = await prisma.refresh.findMany({});
+  } catch (e) {
+    console.error("REFRESH QUERY ERROR", e);
+    return new NextResponse(JSON.stringify(false));
+  }
+  console.info("REFRESH DONE");
+  return new NextResponse(JSON.stringify(true), {
+    status: 200,
+  });
 }
