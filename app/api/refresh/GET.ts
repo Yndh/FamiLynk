8b55ab2255403cf611edce9be77aaea4bb519ad0
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2025-05-24 10:29:10 +0200

    create records on db refresh

--- app/api/refresh/GET.ts
+++ app/api/refresh/GET.ts
@@ -1,12 +1,17 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import type { NextApiResponse } from "next";
+import { randomUUID } from "crypto";
 
 export async function mGET(_req: Request, _res: NextApiResponse) {
   console.info("REFRESH START");
-  let didRefresh;
   try {
-    didRefresh = await prisma.refresh.findMany({});
+    await prisma.refresh.deleteMany({});
+    await prisma.refresh.create({
+      data: {
+        random: randomUUID(),
+      },
+    });
   } catch (e) {
     console.error("REFRESH QUERY ERROR", e);
     return new NextResponse(JSON.stringify(false));
